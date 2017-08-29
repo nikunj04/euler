d9a679bbbf9dfefc4d36eb8c4d3bbecc667ad4cc
Nikunj Raval <nikunj@rushah-mac01.local>
2017-08-29 21:38:44 +0530

    Solution for 49

--- src/main/java/com/raval/euler/problems/Solution49.java
+++ src/main/java/com/raval/euler/problems/Solution49.java
@@ -1,6 +1,7 @@
 package com.raval.euler.problems;
 
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,20 +10,51 @@ import java.util.stream.Stream;
  */
 public class Solution49 {
     public static void main(String[] args) {
-        List<Long> primes1 = Stream.iterate(1486L, l -> l+1)
-                .limit(9000L)
-                .filter(Utility::isPrime)
-                .collect(Collectors.toList());
-        Set<Long> primes = new HashSet<>(primes1);
-        Long primeInQuestion = -1L;
-        Long nextPrime = -1L;
-        for(int i=0;i<primes1.size();i++){
-            primeInQuestion = primes1.get(i);
-            for(int j=i+1;j<primes1.size();j++){
-                nextPrime = primes1.get(j);
-                if(primes1.contains(nextPrime) && primes1.contains(nextPrime + (nextPrime - primeInQuestion)))
-                    System.out.println("Found "+ primeInQuestion + " "+nextPrime + " " + (nextPrime+(nextPrime - primeInQuestion)));
+        Solution49 solution = new Solution49();
+        List<Integer> listPrime = solution.getPrimes();
+        Set<Integer> setPrime = new HashSet<>(listPrime);
+
+
+        for(int i=0;i<listPrime.size();i++){
+            int prime0 = listPrime.get(i);
+            for(int j=i+1;j<listPrime.size();j++){
+                int prime1 = listPrime.get(j);
+                int prime2 = prime1 + (prime1 - prime0);
+                if(setPrime.contains(prime2)){
+                    Set<Integer> digit0 = solution.getDigits(prime0);
+                    Set<Integer> digit1 = solution.getDigits(prime1);
+                    if(digit0.equals(digit1) && digit0.equals(solution.getDigits(prime2)))
+                        System.out.println("Found "+ prime0 + " "+prime1 + " " + prime2);
+                }
             }
         }
+    }
+
+
+    List<Integer> getPrimes(){
+        return Stream.iterate(1000, l -> l+1)
+                .limit(9000)
+                .filter(Utility::isPrime)
+                .collect(Collectors.toList());
+
+    }
+
+    Set<Integer> getDigits(Integer _number){
+        Supplier<Integer> digitSupplier = new Supplier<Integer>() {
+            Integer number = _number;
+            @Override
+            public Integer get() {
+
+                Integer previousNumber = number;
+                number = number / 10;
+                return previousNumber;
+            }
+        };
+
+        return Stream.generate(digitSupplier)
+                .limit(4)
+                .map(number -> number % 10)
+                .collect(Collectors.toSet());
+
     }
 }
